1ea110a0076e8f455f9f2e9a1732bc6fd70d0a8e
Godreigneth <godreignethlevidibia@yahoo.com>
2020-12-02 22:17:00 +0000

    Built the Game Board Start Game

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,15 @@ $('.startButton').click(startGame);
 
 function startGame() {
     console.log('start');
+    $('.startButton').hide();
+    game.clicks = 0;
+    game.pause = false;
+    game.sel = [];
+    game.newArray = boxes.concat(boxes);
+    console.log(game.newArray);
+    $('.game').html('');
+    $.each(game.newArray, function(key,value){
+        console.log(key);
+        console.log(value);
+    })
 }
